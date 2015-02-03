25c5cf16483c1ab2c88e7db022d92f9932259e3f
Juan Francisco Gimenez Silva <juanfgs@localhost.localdomain>
2015-02-03 18:55:08 -0300

    added schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150122154433) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email",      null: false
+    t.string   "password",   null: false
+    t.string   "salt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
