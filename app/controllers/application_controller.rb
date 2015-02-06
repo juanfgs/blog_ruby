8f90c764955e4ccd9836625cb6fd50ba625c274f
Juan Francisco Gimenez Silva <juanfgs@localhost.localdomain>
2015-02-06 09:45:16 -0300

    added authentication methods for active admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,26 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+  
+  def current_admin_user
+    if session[:user_id]
+      @current_user = User.find session[:user_id]
+    else
+      return false
+    end
+  end
+
+  def authenticate_admin_user
+    unless current_admin_user
+      access_denied
+    end
+  end
+  
+  def access_denied
+    redirect_to '/', flash: {error: "Access Denied"}
+  end
+
+  
 end
