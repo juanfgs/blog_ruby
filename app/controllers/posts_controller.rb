f79881af6843e6f0ea669fb4911f830cdd5428db
Juan Francisco Gimenez Silva <juanfgs@localhost.localdomain>
2015-02-06 13:31:00 -0300

    changed index method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,5 @@
 class PostsController < ApplicationController
   def index
-
-    
-    @posts = Post.paginate(:page => params[:page], :per_page => 15)
-    
-
+    @posts = Post.all.page params[:page]
   end
 end
