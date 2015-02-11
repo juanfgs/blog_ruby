c0bfbf3ebe323a0c15f16c5f838f941b5d67742e
Juan Francisco Gimenez Silva <juanfgs@localhost.localdomain>
2015-02-11 10:21:00 -0300

    changes to schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150206144954) do
+ActiveRecord::Schema.define(version: 20150206191231) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -42,17 +42,6 @@ ActiveRecord::Schema.define(version: 20150206144954) do
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
-  create_table "profiles", force: :cascade do |t|
-    t.text     "summary"
-    t.string   "twitter"
-    t.string   "facebook"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "user_id"
-  end
-
-  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
-
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email",      null: false
@@ -60,6 +49,9 @@ ActiveRecord::Schema.define(version: 20150206144954) do
     t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "summary"
+    t.string   "facebook"
+    t.string   "twitter"
   end
 
 end
