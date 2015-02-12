8f3afc63af6af6f4263e195b2e20166ad3cca332
Juan Francisco Gimenez Silva <juanfgs@localhost.localdomain>
2015-02-12 12:07:08 -0300

    added paperclip sizes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :large =>  "750x370#", :medium => "300x150>", :thumb => "100x50>" }, :default_url => "/images/:style/missing.png"
   validates :image, presence: true
   validates_attachment_content_type :image, :content_type => /^image\/(jpg|jpeg|png)$/
   validates_attachment_size :image, :less_than => 2.megabytes
