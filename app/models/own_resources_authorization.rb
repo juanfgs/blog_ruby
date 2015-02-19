21d5c66ac69fe6a8bb77675f5ff8104bb00f196d
Juan Francisco Gimenez Silva <juanfgs@localhost.localdomain>
2015-02-19 14:04:03 -0300

    removed redundant return values

--- app/models/own_resources_authorization.rb
+++ app/models/own_resources_authorization.rb
@@ -6,14 +6,10 @@ class OwnResourcesAuthorization < ActiveAdmin::AuthorizationAdapter
       # Only let the author update and delete posts
       if action == :update || action == :destroy
         subject.user == user
-      else
-        true
       end
     when normalized(User)
       if action == :update || action == :destroy
         subject == user
-      else
-        true
       end
     else
       true
